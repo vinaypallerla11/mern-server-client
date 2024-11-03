4f391495e3753971bbe9b06c7437b7acd001eee5
vinaypallerla11 <vinaypallerla96@gmail.com>
2024-11-03 17:52:03 +0530

    add styles

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,40 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useCart } from '../../context/CartContext';
-import Cookies from 'js-cookie';
+import { MdOutlineFormatLineSpacing } from 'react-icons/md';
 import './index.css';
+import Cookies from 'js-cookie';
 
 const NavBar = () => {
   const navigate = useNavigate();
-  const { cart } = useCart();
-  const cartCount = cart.reduce((count, item) => count + item.quantity, 0);
+  const [dropdownVisible, setDropdownVisible] = useState(false);
 
   const onClickLogout = () => {
     Cookies.remove('jwt_token');
     navigate('/', { replace: true });
   };
 
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
   return (
     <div className="containerSection">
       <div className="content-nav">
-        <Link to="/">
-          <h1>
-            <img src="https://res.cloudinary.com/ddehbjyiy/image/upload/v1716325832/luxury-letter-v-logo-v-logotype-for-elegant-and-stylish-fashion-symbol-vector_czeu0g.jpg" alt="vlogo" className="img-content" />
-            <img src="https://res.cloudinary.com/ddehbjyiy/image/upload/v1704763899/VTrendz_gfuzbu.webp" alt="VTrendz" className="img-content" />
-          </h1>
-        </Link>
+        <div>
+          <Link to="/">
+            <img
+              src="https://res.cloudinary.com/ddehbjyiy/image/upload/v1716325832/luxury-letter-v-logo-v-logotype-for-elegant-and-stylish-fashion-symbol-vector_czeu0g.jpg"
+              alt="vlogo"
+              className="img-content"
+            />
+          </Link>
+          <Link to="/">
+            <img
+              src="https://res.cloudinary.com/ddehbjyiy/image/upload/v1704763899/VTrendz_gfuzbu.webp"
+              className="img-content"
+              alt="avatar"
+              onClick={() => navigate('/india', { replace: true })}
+            />
+          </Link>
+        </div>
         <div className="nav-container">
-          <ul>
+          <ul className="desktop-menu">
             <Link to="/"><li>Home</li></Link>
             <Link to="/about"><li>About</li></Link>
             <Link to="/products"><li>Products</li></Link>
-            <Link to="/cart"><li>Cart ({cartCount})</li></Link>
+            <Link to="/cart"><li>Cart</li></Link>
+            <button type="button" onClick={onClickLogout} className="button-container2">
+              Logout
+            </button>
           </ul>
-          <button type="button" onClick={onClickLogout} className="button-container2">
-            Logout
-          </button>
+          <MdOutlineFormatLineSpacing className="mobile-menu-icon" onClick={toggleDropdown} />
         </div>
       </div>
+      {dropdownVisible && (
+        <div className="dropdown">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/products">Products</Link></li>
+            <li><Link to="/cart">Cart</Link></li>
+            <li>
+              <button type="button" onClick={onClickLogout} className="button-container2">
+                Logout
+              </button>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
